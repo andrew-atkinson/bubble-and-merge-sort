671c91d13e5a944291d735a5ca4a3de2a3a42cb8
andrew-atkinson <blind.sam@gmail.com>
2017-06-10 21:12:43 -0400

    merge sort working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function stepBubble(arr) {
   let returnArr = [];
   var changed = false;
@@ -11,12 +13,43 @@ function stepBubble(arr) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         changed = false;
       }
-    }
-    // in order to create a new string and avoid create by reference.
+    } // in order to create a new string and avoid create by reference.
     returnArr.push(arr.concat());
   }
   return returnArr;
 }
+
+
+function mergeSort(arr) {
+  // halve arr
+  if (arr.length < 2) return arr;
+
+  let l = arr.slice(0, Math.floor(arr.length / 2));
+  let r = arr.slice(Math.floor(arr.length / 2), arr.length);
+  // then merge pairs (once pair base is met)
+  return merge(mergeSort(l), mergeSort(r));
+}
+
+function merge(l, r) {
+  let result = [];
+
+  // where's still left or right left
+  while (l.length && r.length) {
+    // if the left is lower than right, push it on the RESULT.
+    if (l[0] < r[0]) {
+      result.push(l.shift());
+    } else {
+      result.push(r.shift());
+    }
+  }
+  return result.concat(l).concat(r);
+}
+
+let arr = randomArr(100);
+console.log('original: ' + arr);
+console.log('logged: ' + mergeSort(arr));
+console.log(stepBubble(randomArr(20)));
+
 
 function randomArr(len) {
   let arr = [];
@@ -30,7 +63,3 @@ function randomArr(len) {
   }
   return arr
 }
-
-let what = stepBubble(randomArr(20));
-
-console.log(what);
